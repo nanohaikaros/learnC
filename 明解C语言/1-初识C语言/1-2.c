b63e46dd7c1a21f668910a0dd4f788a7e1d47cae
ikaros <1371689491@qq.com>
2017-10-14 15:06:33 +0800

    01

---
+++ 明解C语言/1-初识C语言/1-2.c
@@ -0,0 +1,12 @@
+/*
+    计算并显示15减去37的差
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d", 15 - 37);
+
+    return (0);
+}
---
+++ 明解C语言/1-初识C语言/1-3.c
@@ -0,0 +1,12 @@
+/*
+    人性化地显示15与37的和
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("15与37的和是%d。\n", 15 + 37);
+
+    return (0);
+}
