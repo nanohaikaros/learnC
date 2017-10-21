80a15038c8d1070603a864709f17057cb5753101
ikaros <1371689491@qq.com>
2017-10-21 13:24:07 +0800

    7-2 整型和字符型

---
+++ 明解C语言/7-基本数据类型/7-1.c
@@ -0,0 +1,18 @@
+/*
+    本书设定的<linits.h>的部分内容
+*/
+
+#define UCHAR_MAX   255U        //unsigned char的最大值
+#define SCHAR_MIN   -128        //signed char的最小值
+#define SCHAR_MAX   +127        //signed char的最大值
+#define CHAR_MIN    0           //char的最小值
+#define CHAR_MAX    UCHAR_MAX   //char的最大值
+#define SHRT_MIN    -32767      //short int的最小值
+#define SHRT_MAX    +32767      //short int的最大值
+#define USHRT_MAX   65535U      //unsigned short的最大值
+#define INT_MIN     -32768      //int的最小值
+#define INT_MAX     +32767      //int的最大值
+#define UINT_MAX    65535U      //unsigned int的最大值
+#define LONG_MIN    -2147483648L    //long int的最小值
+#define LONG_MAX    +2147483647L    //long int的最大值
+#define ULONG_MAX   42949672950L    //unsigned long的最大值
