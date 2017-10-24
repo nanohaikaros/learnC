791e8f41f89cc76aa158c04301c1dde9703bcd9b
ikaros <1371689491@qq.com>
2017-10-24 13:46:19 +0800

    6-2 函数设计

--- 明解C语言/6-函数/练习6-10.c
+++ 明解C语言/6-函数/练习6-10.c
@@ -5,30 +5,37 @@
 
 #include <stdio.h>
 
-//把2行3列的矩阵ma和mb的和保存到mc中
-void mul(int ma[2][3], int mb[3][2], int mc[2][2])
+void mul(const int ma[2][3], const int mb[3][2], int mc[2][2])
 {
-    int i, j;
-
+    int i, j, k;
     for (i = 0; i < 2; i++)
-        for (j = 0; j < 2; j++)
-            mc[i][j] = ma[i][j] * mb[i][j];
+        for (j = 0; j < 2; j++) {
+            for(k = 0; k < 3; k++) {
+                mc[i][j] += ma[i][k] * mb[k][j];
+            
+                printf("mc[%d][%d] += ma[%d][%d] * mb[%d][%d]\n", i, j, i, k, k, j);
+            }  
+            
+            printf("mc[%d][%d] = %d\n", i, j, mc[i][j]);
+            putchar('\n');
+        } 
 }
+
 
 int main(void)
 {
     int i, j;
-    int ma[2][3] = { {1, 2, 3}, {4, 5, 6} };
-    int mb[3][2] = { {6, 3}, {5, 1}, {1, 2} };
-    int mc[2][2] = { 0 };
-
+    int ma[2][3] = {{1, 2, 3}, {4, 5, 6}}; 
+    int mb[3][2] = {{6, 3}, {4, 5}, {1, 2}};
+    int mc[2][2] = { 0 }; 
+    
     mul(ma, mb, mc);
-
+    
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++)
             printf("%3d", mc[i][j]);
         putchar('\n');
-    }
-
+    }   
+    
     return (0);
 }
--- 明解C语言/6-函数/练习6-11.c
+++ 明解C语言/6-函数/练习6-11.c
@@ -1,72 +1,72 @@
 /*
-    ��дһ�γ���ʹ�ö�ά�������5��ѧ��3����Ŀ�����ġ���ѧ��Ӣ��ķ�����������´�����
-    ��1������ÿ�Ƶ���߷֡�
-    ��2������ÿ��ѧ��3����Ŀ��ƽ���֡�
+    编写一段程序，使用二维数组操作5名学生3个科目（语文、数学、英语）的分数，完成如下处理。
+    例1：计算每科的最高分。
+    例2：计算每名学生3个科目的平均分。
 */
 
 #include <stdio.h>
 
-#define NUMBER 5
-#define COUNT 3
-
-int max_of(int vc[], int no)
+// 计算每科最高分，保存到数组s 
+void h_score(int m[5][3], int s[3])
 {
-    int i;
-    int max = vc[0];
-    for (i = 1; i < no; i++)
-        if (vc[i] > max)
-            max = vc[i];
-    return (max);
+    int i, j;
+    
+    for (i = 0; i < 3; i++) {
+        
+        s[i] = m[0][i];
+        
+        for (j = 1; j < 5; j++)
+            s[i] = s[i] > m[j][i] ? s[i] : m[j][i];
+    }             
 }
 
-double avg_of(double vc, int no)
+// 计算每名学生3个科目的平均分，保存到数组a
+void a_score (int m[5][3], float a[5])
 {
-    return (vc / no);
+    int i, j;
     
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 3; j++)
+            a[i] += m[i][j];   
+        a[i] /= 3;
+    } 
 }
 
 int main(void)
 {
     int i, j;
-    int vc[NUMBER][COUNT];
-    int chi[NUMBER];
-    int eng[NUMBER];
-    int mat[NUMBER];
-    int max_c, max_e, max_m;
-    int avg[NUMBER];
-
-
-    printf("������%d��ѧ���ķ�����\n", NUMBER);
-    for (i = 0; i < NUMBER; i++) {
-        puts("���ġ���ѧ��Ӣ�");
-        for (j = 0; j < COUNT; j++) {
-            putchar('=');
-            scanf("%d", &vc[i][j]);
+    int m[5][3];
+    int max[3];
+    float ave[5] = {0};
+    
+    for (i = 0; i < 5; i++) {
+        printf("请输入第%d名同学的成绩：\n", i + 1);
+        for (j = 0; j < 3; j++) {
+            switch (j) {
+                case 0 : printf("语文:"); scanf("%d", &m[i][j]); break;
+                case 1 : printf("数学:"); scanf("%d", &m[i][j]); break;
+                case 2 : printf("英语:"); scanf("%d", &m[i][j]); break;
+            }
         }
     }
-
-    for (i = 0; i < NUMBER; i++) {
-        chi[i] = vc[i][0];
-        eng[i] = vc[i][1];
-        mat[i] = vc[i][2];
-
+    
+    h_score(m, max);
+    a_score (m, ave);
+    
+    putchar('\n');
+    
+    for (j = 0; j < 3; j++) {
+        switch (j) {
+                case 0 : printf("语文的最高分是:%d\n", max[j]); break;
+                case 1 : printf("数学的最高分是:%d\n", max[j]); break;
+                case 2 : printf("英语的最高分是:%d\n", max[j]); break;
+            }
     }
-
-    max_c = max_of(chi, NUMBER);
-    max_e = max_of(eng, NUMBER);         
-    max_m = max_of(mat, NUMBER);         
-
-    printf("���ĵ���߷�=%d\n", max_c);
-    printf("Ӣ�����߷�=%d\n", max_e);
-    printf("��ѧ����߷�=%d\n", max_m);
-
     
-        for (i = 0; i < NUMBER; i++){
-            avg[i] = vc[i][0] + vc[i][1] + vc[i][2];
-            printf("%dƽ����:%.2f\n", i, avg_of(avg[i], COUNT));
-        }
+    putchar('\n');
     
-    
-
-    return (0);
+    for (i = 0; i < 5; i++)
+        printf("第%d名学生的平均分数是:%.2f\n", i + 1, ave[i]);
+        
+    return (0);      
 }
